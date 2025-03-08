928cd9fd74e4ae46e0e01fae92749c21f0ff8938
Emil Petkov <studio.vodopad@gmail.com>
2025-03-08 15:35:24 +0200

    add zad2

---
+++ zad2_for_training/DoublyLinkedList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2_for_training
+{
+    internal class DoublyLinkedList<T>
+    {
+        public DoublyLinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public DoublyNode<T>? Head { get; set; } // Начален (първи) възел в списъка
+        public DoublyNode<T>? Tail { get; set; } // Краен (последен) възел в списъка
+
+
+        // Добавяне на елемент в началото на списъка
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> newNode = new DoublyNode<T>(data);
+            if (Head == null) // Ако списъкът е празен
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode.Next = Head;
+                Head.Previous = newNode;
+                Head = newNode;
+            }
+        }
+
+        // Премахване на елемент от началото на списъка
+        public T RemoveFirst()
+        {
+            if (Head == null)
+                throw new InvalidOperationException("Списъкът е празен");
+
+            T removedData = Head.Data; // Запазване на премахнатите данни
+            Head = Head.Next; // Пренасочване на главата към следващия възел
+
+            if (Head != null)
+                Head.Previous = null;
+            else
+                Tail = null; // Ако списъкът стане празен
+
+            return removedData;
+        }
+
+        // Търсене на елемент по стойност
+        public bool Contains(T value)
+        {
+            DoublyNode<T>? current = Head;
+            while (current != null)
+            {
+                if (current.Data.Equals(value))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        // Проверка дали списъкът е празен
+        public bool IsEmpty()
+        {
+            return Head == null;
+        }
+
+        // Добавяне на елемент в края на списъка
+        public void AddLast(T data)
+        {
+            DoublyNode<T> newNode = new DoublyNode<T>(data);
+            if (Tail == null) // Ако списъкът е празен
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                newNode.Previous = Tail;
+                Tail = newNode;
+            }
+        }
+
+        // Премахване на елемент от края на списъка
+        public T RemoveLast()
+        {
+            if (Tail == null)
+                throw new InvalidOperationException("Списъкът е празен");
+
+            T removedData = Tail.Data; // Запазване на премахнатите данни
+            Tail = Tail.Previous; // Пренасочване на опашката към предходния възел
+
+            if (Tail != null)
+                Tail.Next = null;
+            else
+                Head = null; // Ако списъкът стане празен
+
+            return removedData;
+        }
+
+    }
+}
+
---
+++ zad2_for_training/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace zad2_for_training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+            for (int i = 2; i <= 50; i++)
+            {
+                if (IsPrime(i))
+                {
+                    list.AddLast(i);
+                }
+            }
+
+            Console.WriteLine();
+            PrintList<int>(list.Head);
+            Console.WriteLine();
+            ReversePrintList<int>(list.Tail);
+        }
+
+        static void PrintList<T>(DoublyNode<T>? head)
+        {
+            
+        }
+
+        static void ReversePrintList<T>(DoublyNode<T>? tail)
+        {
+            
+        }
+
+        static bool IsPrime(int number)
+        {
+            
+        }
+    }
+}
